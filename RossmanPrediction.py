283bee96b6a75ebadf929ed6283d57c4546f270f
Mohammad Farahani <80159489+MohFarahani@users.noreply.github.com>
2021-08-11 17:03:37 +0200

    add clean-fillna

--- RossmanPrediction.py
+++ RossmanPrediction.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
 import sys
+import pickle
+import xgboost
+
 
 PATH_STORE = 'modified_store.csv'
-PATH_MODEL = 'xgb.pickle'
+PATH_MODEL = 'xgb.pickle.dat'
 
 def metric(preds, actuals):
     preds = preds.reshape(-1)
@@ -13,26 +16,53 @@ def metric(preds, actuals):
 
 def evaluate(path_X_csv,path_y_csv):
     train = pd.read_csv(path_X_csv)
-    mask_open = train['Open']==1
-    train = train[mask_open]
     y_real = pd.read_csv(path_y_csv)
-    y_real = y_real[mask_open]
+
+    if 'Open' in train.columns:
+        mask_open = train['Open']==1
+        train = train[mask_open]
+        y_real = y_real[mask_open]
     y_predict = predict(train)
     RMSPE = metric(y_predict, y_real)
     print("RMSPE: ",RMSPE)
     return RMSPE
 
 def clean(df):
-    pass
+    if 'StateHoliday' in df.columns:
+        df.loc[df['StateHoliday']==0,'StateHoliday']='0'
+        df.loc[df['StateHoliday']=='0','StateHoliday']='0'
 
 def fillna(df):
-    pass
+    def fillna_mean(df,columns):
+        for col in columns:
+            if col in df.columns:
+                mean_value = int(df[col].mean())
+                df.loc[:,col].fillna(value=mean_value,inplace=True)
+        return df
+
+    def fillna_most(df,columns):
+        for col in columns:
+            if col in df.columns:
+                most_value = df[col].value_counts().idxmax()
+                df.loc[:,col].fillna(value=most_value,inplace=True)
+        return df
+    columns_mean = ['DayOfWeek','Customers']
+    df = fillna_mean(df,columns_mean)
+
+    columns_most = ['Promo','SchoolHoliday','StateHoliday']
+    train = fillna_most(df,columns_most)
 
 def encoding(df):
     pass
 
 def new_features(df):
     pass
+
+def drop_columns(df,columns):
+    for col in columns:
+        if col in df.columns:
+            df.drop(columns=[col],inplace=True)
+    return df
 
 def predict(train):
     store = pd.read_csv(PATH_STORE)
@@ -41,8 +71,10 @@ def predict(train):
     train_full = pd.merge(train,store, on='Store', how='inner')
     train_full = encoding(train_full)
     train_full = new_features(train_full)
+    drop_columns_list = ['Store','Customer','Date','Open']
+    train_full = drop_columns(drop_columns_list)
     train_full.dropna(inplace=True)
-    xgb = loadXGB
+    xgb = pickle.load(open(PATH_MODEL, "rb"))
     return xgb.predict(train)
 
 if __name__ == "__main__":
