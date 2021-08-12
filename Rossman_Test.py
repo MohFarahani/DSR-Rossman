e705ad13a9797e1338002b0a7da618520d6d0078
Mohammad Farahani <80159489+MohFarahani@users.noreply.github.com>
2021-08-12 05:09:53 +0200

    Create Rossman Class

---
+++ Rossman_Test.py
@@ -0,0 +1,11 @@
+import sys
+from train_class import Rossman
+
+# PATH to Input data
+PATH_STORE_MODIFIED = "data/store_modified.csv"
+MODEL_NAME = "XGBoost.txt"
+
+if __name__ == "__main__":
+    path_test = sys.argv[1]
+    rosmann = Rossman()
+    rosmann.testing(path_test,PATH_STORE_MODIFIED,MODEL_NAME)
